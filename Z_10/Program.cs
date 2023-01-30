25e1a64bc29828c492a82625a24c7d816ab109dc
Sergei Vasiliev <9670105747@mail.ru>
2023-01-30 17:07:19 +0700

    Вторая цифра трехзначного числа

--- Z_10/Program.cs
+++ Z_10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает 
+//на вход трехзначное число и на выходе 
+//показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number <= 999)
+{
+    Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+}
+else
+	Console.WriteLine("Число не трёхзначное");
+
+Console.WriteLine();
