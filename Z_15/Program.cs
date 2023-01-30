6844f04049d48a7e9ee57c8787c47c17466ab7d1
Sergei Vasiliev <9670105747@mail.ru>
2023-01-30 18:02:03 +0700

    Задача на определение выходного дня

--- Z_15/Program.cs
+++ Z_15/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+Console.Clear();
+string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.WriteLine("Введите день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    number = number - 1;
+    if ((5 <= number) && (number<= 6))Console.WriteLine($" {daysOfWeek[number]} выходной ");
+    else Console.WriteLine(daysOfWeek[number]);
+}
+else
+    Console.WriteLine("Не является днем недели");
+
+Console.WriteLine();
