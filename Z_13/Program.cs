4fbfae92b0896fe030ee4badad9ad39392293e62
Sergei Vasiliev <9670105747@mail.ru>
2023-01-30 17:25:48 +0700

    Есть третья цифра в случайном числе

--- Z_13/Program.cs
+++ Z_13/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что 
+//третьей цифры нет.
+
+Console.Clear();
+
+int Number = new Random().Next(1, 2000);
+Console.WriteLine();
+Console.WriteLine($"Число: {Number}");
+
+if (Number / 100 > 0)
+{
+    if(Number > 999)
+    {
+        Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 10 % 10}");
+    }
+    else
+    {
+        Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number % 10}");
+    }
+}
+else
+{
+    Console.WriteLine($"У числа {Number} нет третьей цифры");
+}
+
+Console.WriteLine();
+
