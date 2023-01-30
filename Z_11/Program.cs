55e2d50ce08a5943eeff202ec782443c9aa38e58
Sergei Vasiliev <9670105747@mail.ru>
2023-01-30 17:08:42 +0700

    Минимум и максимум двухзначного числа

---
+++ Z_11/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и 
+// показывает наибольшую цифру числа.(минимум, максимум, равны)
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Console.Clear();
+
+int Number = new Random().Next(10, 100);
+int Max = 0;
+int Min = 0;
+
+int MaxNum()
+{
+    int Num1 = Number / 10;
+    int Num2 = Number % 10;
+    if (Num1 > Num2)
+    {
+        Max = Num1;
+    }
+    else
+    {
+        Max = Num2;
+    }
+    return Max;
+}
+int MinNum()
+{
+    int Num1 = Number / 10;
+    int Num2 = Number % 10;
+    if (Num1 < Num2)
+    {
+        Min = Num1;
+    }
+    else
+    {
+        Min = Num2;
+    }
+    return Min;
+}
+
+MaxNum();
+MinNum();
+if(Min != Max) 
+{
+    Console.WriteLine($"В числе {Number} max = {Max}, min = {Min}");
+}
+else
+{
+     Console.WriteLine($"В числе {Number} цифры равны");
+}
+
+Console.WriteLine();
